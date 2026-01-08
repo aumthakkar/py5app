9885b9763cb5945e140119267c0d670edcd160da
aumthakkar <aumthakkar@yahoo.co.uk>
2026-01-08 19:31:36 +0000

    first commit with src folder and Dockerfile

---
+++ src/py5app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+import time
+import socket
+
+
+app = Flask(__name__)
+
+@app.route('/api/v1/healthz')
+def healthz():
+    return jsonify({
+        "status" : "up"
+    })
+
+@app.route('/api/v1/info')
+def info():
+    return jsonify({
+        "time" : time.ctime(),
+        "hostname" : socket.gethostname(),
+        "message" : "You are doing great, Pranav!"
+    })
+
+if __name__ == '__main__':
+    app.run("0.0.0.0")
