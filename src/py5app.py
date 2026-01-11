6824c265549a33f70d8a159abd7093320e011025
aumthakkar <aumthakkar@yahoo.co.uk>
2026-01-11 20:51:36 +0000

    added github workflow

--- src/py5app.py
+++ src/py5app.py
@@ -16,8 +16,7 @@ def info():
     return jsonify({
         "time" : time.ctime(),
         "hostname" : socket.gethostname(),
-        "message" : "You are doing great, man!",
-        "env" : "dev"
+        "message" : "You are doing great, man!"
     })
 
 if __name__ == '__main__':
